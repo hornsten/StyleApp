a6484ed48977037cda4ab0bb0c99576f4b568c94
FH <FH@Fionas-MacBook-Air.local>
2017-03-16 13:43:04 -0500

    prepared way for private chats

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -22,16 +22,56 @@
 		$.each(rooms, function(key, value) {
 			if(value == current_room){
 				$('#rooms').append('<div>' + value + '</div>');
+                $('#current-room').append('<div>' + value + '</div>');
 			}
 			else {
+                console.log('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
 				$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
 			}
 		});
 	});
 
+    // $('#addfile').on('submit', function(e){
+    //    console.log("in here");
+    //     // var data = e.originalEvent.target.files[0];
+    //     data = $('#uploadfile').val();
+    //     console.log(data);
+    //     readThenSendFile(data);     
+    //     e.preventDefault(); 
+    // });
+
+    // $("#uploadfile").on('change',function(e){
+    
+    //    console.log("in here",  $("#uploadfile").val());
+    //     // // var data = e.originalEvent.target.files[0];
+    //     // data = $('#uploadfile').val();
+    //     // console.log(data);
+
+    //     // readThenSendFile(data); 
+    //     socket.emit('base64 file', $("#uploadfile").val());    
+    //     e.preventDefault(); 
+
+    // });
+
+    // function readThenSendFile(data){
+
+    //         var msg ={};
+    //         // msg.username = username;
+    //         msg.file = evt.target.result;
+    //         msg.fileName = data.name;
+    //         console.log(msg);
+    //         socket.emit('base64 file', msg);
+
+    // }
+
 	function switchRoom(room){
 		socket.emit('switchRoom', room);
 	}
+
+    function privateRoom(chatuser){
+		socket.emit('privateRoom',chatuser);
+	}
+ 
 
 	// on load of page
 	$(function(){
@@ -51,5 +91,22 @@
 			}
 		});
 	});
+
+    $(document).ready( function(){
+        //get users names and display
+        $('#users').html();
+        console.log("currentRoom", $('#current-room').val());
+        // hardcoded "room1"
+        $('#users').append('<ul>');
+        $.get('/chat/user/room1', function(data){
+            // console.log(data);
+            for (var i = 0; i < data.length; i++){
+                console.log('<li><a href="#" onclick="privateRoom(\''+data[i].username+'\')"">'+data[i].username+'</a></li>');
+                $('#users').append('<li><a href="#" onclick="privateRoom(\''+data[i].username+'\')">'+data[i].username+'</a></li>');
+            
+            }
+        })
+        $('#users').append('</ul>');
+    })
 
 
