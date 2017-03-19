a0179bdeeef5279664d21d2a5be4b71ccc469168
FH <FH@Fionas-MacBook-Air.local>
2017-03-19 11:07:34 -0500

    got updatedata to work by passing in the store to the chathelper function and putting the socket listener in contstuctor. Still issues displaying msg property but getting there

--- components/ChatSection.js
+++ components/ChatSection.js
@@ -1,16 +1,53 @@
 
 import ReactDOM from "react-dom";
 import React from "react";
-import Chat from "./Chat.js";
+// import Chat from "./Chat.js";
 import CurrentUserAndRoom from "./CurrentUserAndRoom.js";
-import ChatInput from "./ChatInput.js";
+// import ChatInput from "./ChatInput.js";
+// import ChatHistory from "./ChatHistory.js";
+import {connect } from 'react-redux';
+import store from './Redux/redux.js';
+import chathelper from "../app/utils/chathelper.js";
+import io from 'socket.io-client'
+// let socket = io(`http://localhost:8000`)
+var socket = io.connect();
 
 
 class ChatSection extends React.Component {
     constructor(props) {
         super(props);
-    }
 
+        // this.updatechat = this.updatechat.bind(this);
+        chathelper.updatechat_listener(store);
+        // socket.on('updatechat', (username, data) => {   
+        //      console.log("socket", username, data);
+        //     this.updatechat(username, data);
+        // })
+    }
+    // updatechat(username, data){
+    //         console.log("callint this udate caht function", username, data);
+    //         store.dispatch({ 
+    //             type: 'CHAT_USER',
+    //             chatuser: username
+    //         },
+    //         { 
+    //             type: 'CHAT_MSG',
+    //             chatmsg: data
+    //         })
+    // }
+    addMessage(message) {
+        //*** put all sockets in separate helper file  ***/
+        // tell server to execute 'sendchat' and send along one parameter
+        // socket.emit('sendchat', message);
+        chathelper.sendchat(message);
+    }
+    updateMessage(e){
+        store.dispatch({ 
+            type: 'ADD_MESSAGE',
+            message: e.target.value
+        })
+
+    }
     render() {
         return (<div>
                     <div className="row">
@@ -18,13 +55,36 @@ class ChatSection extends React.Component {
                     </div>
                     <hr />
                     <div className="row">
-                        <Chat />
+                        <div className="col-xs-12 col-md-12">
+                            History Goes in here -- update when message added
+                            <div className="chatbox">
+                             need to append data
+                                <div id="conversation">YSER {this.props.chatuser}</div>
+                                <div >MSG  {this.props.chatmsg}</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="row">
-                        <ChatInput />
+                        <div className="col-md-2"></div>
+                            <div className="col-md-8">
+
+                                <input type="text" value={this.props.message}  onChange={this.updateMessage}  className="form-control"  />
+                                <button type="button" onClick={() => this.addMessage(this.props.message)}>Add Message</button>
+                            </div>
+                        <div className="col-md-2"></div>
                     </div>
                 </div>);
     }
 };
 
-export default ChatSection;
+const mapStateToProps = (store,ownProps) => {
+    return {
+        message: store.chatState.message,
+       
+        chatmsg: store.chatState.chatmsg,
+         chatuser: store.chatState.chatuser
+    }
+};
+
+export default connect(mapStateToProps)(ChatSection);
+// export default ChatSection;
