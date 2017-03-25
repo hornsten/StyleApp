0924b5e221a5c00e60e4a4a1717a42eb11a77147
mitalimn <mitalinaik@yahoo.com>
2017-03-25 15:11:55 -0500

    heroku added

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 // connect to the database
 
-// var MONGODB_URI = 'mongodb://heroku_wk1d2fsz:t9gn1gkjf21obtkimanj9lgmb4@ds143000.mlab.com:43000/heroku_wk1d2fsz';
+var MONGODB_URI = 'mongodb://heroku_6mzw55vf:t095a30cer0nuka2l01e11vmf5@ds141960.mlab.com:41960/heroku_6mzw55vf';
 // var connectionString = process.env.MONGODB_URI | 'mongodb://localhost:27017/styleapp';
-// var connection = mongoose.connect(process.env.MONGODB_URI);
-var connection = mongoose.connect('mongodb://localhost:27017/styleapp');
+var connection = mongoose.connect(process.env.MONGODB_URI);
+// var connection = mongoose.connect('mongodb://localhost:27017/styleapp');
 // export the connection
 
 // var connection = mongoose.connect(connectionString);
