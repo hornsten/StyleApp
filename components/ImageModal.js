fddd50adc547581c3bc2b6ba05369b4b06d86f5b
mitalimn <mitalinaik@yahoo.com>
2017-03-31 15:39:56 -0500

    image modal

---
+++ components/ImageModal.js
@@ -0,0 +1,36 @@
+
+import ReactDOM from "react-dom";
+import React from "react";
+import ReactModal from 'react-modal';
+
+
+class ImageModal extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            showModal:false
+        };
+    }
+    setModalState(showModal){
+        this.setState({
+            showModal:showModal
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <img src={this.props.src} onClick={this.setModalState.bind(this,true)} />
+
+                <ReactModal isOpen={this.state.showModal} >
+
+                <img src={this.props.src} onClick={this.setModalState.bind(this,false)} />
+                </ReactModal>
+            </div>
+        )
+    }
+}
+
+export default ImageModal;
