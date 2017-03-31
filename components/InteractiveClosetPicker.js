db0f1c0434205bac188be432f7e895b5eab65fe9
FH <FH@Fionas-MacBook-Air.local>
2017-03-30 20:19:01 -0500

    changed interactive update

--- components/InteractiveClosetPicker.js
+++ components/InteractiveClosetPicker.js
@@ -109,8 +109,11 @@ console.log(prevProps.index, this.props.index)
 console.log(prevProps.items , this.props.items) 
 console.log(prevProps.itemid, this.props.itemid)
 if ((prevProps.index !== this.props.index)){
-   console.log("this prpos**********d");
+  //  console.log("this prpos**********d");
    this.updateClothesBin(this.props.index, this.props.items, this.props.itemid);
+} else if ((prevProps.items !== this.props.items)){
+   console.log("this prpos**********d");
+   this.updateClothesBin(this.props.index, this.props.items, this.props.itemid)
 }
 // console.log("state of store", store.getState())
 // var Items = this.props.items;
